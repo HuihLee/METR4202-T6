530baa7f116a0f6fa8429e6211921a3b2673dc15
CALLUM_MCMAHON <callum.mcmahon@student.uq.edu.au>
2021-09-29 14:11:16 +1000

    locations of all of the cube homes

--- kinematics/main.py
+++ kinematics/main.py
@@ -1,5 +1,12 @@
 import numpy as np
 import modern_robotics as mr
+from enum import Enum
+
+class Colour(Enum):
+    RED = 0
+    GREEN = 1
+    BLUE = 2
+    PURPLE = 3
 
 """ Helper functions"""
 # Create a 3x3 rotation matrix
@@ -168,6 +175,70 @@ def testInverseKinematics(M, Screws):
     print("Did it work? {}".format(success))
     print("arm angles = {}".format(toDegrees(thetasEnd)))
 
+def initialiseCubeAngles(cubeHomeAngles):
+    cubeHeight = 16 # mm
+    cubePositions = np.array([
+        [[-125, -25, cubeHeight],
+         [-125, -75, cubeHeight],
+         [-175, -75, cubeHeight],
+         [-175, -25, cubeHeight]],
+        [[-25, -125, cubeHeight],
+         [-25, -175, cubeHeight],
+         [-75, -175, cubeHeight],
+         [-75, -125, cubeHeight]],
+        [[75, -125, cubeHeight],
+         [75, -175, cubeHeight],
+         [25, -175, cubeHeight],
+         [25, -125, cubeHeight]],
+        [[175, -25, cubeHeight],
+         [175, -75, cubeHeight],
+         [125, -75, cubeHeight],
+         [125, -25, cubeHeight]],
+    ])
+
+    # Inverse Kinematics for a point and orientation
+    orientation = rot(np.array([0, 0, 1]), 0)
+    colour = 0
+    colours = 4
+    while colour < colours:
+        position = 0
+        positions = 4
+        while position < positions:
+            translation = np.array([[cubePositions[colour][position][0],
+                                     cubePositions[colour][position][1],
+                                     cubePositions[colour][position][2]]])
+
+            # Desired configuration
+            T_des = np.concatenate((np.concatenate((orientation, translation.T), axis=1),
+                                    np.array([[0, 0, 0, 1]])))
+
+            # Make an initial guess and specify the acceptable error
+            if colour == 0 or colour == 1:
+                thetasInit = np.array([np.pi * 9.0 / 10, -np.pi * 2 / 3, 0, 0])
+            else:
+                thetasInit = np.array([-np.pi * 9.0 / 10, -np.pi * 2 / 3, 0, 0])
+
+            errorOmega = 0.001
+            errorPosition = 0.001
+
+            thetasEnd, success = mr.IKinSpace(Screws.T, M, T_des, thetasInit, errorOmega, errorPosition)
+            #thetasEnd = limitAngles(thetasEnd)
+            T_calc = mr.FKinSpace(M, Screws.T, thetasEnd)
+            print(success)
+
+            cubeHomeAngles[colour][position] = thetasEnd
+            position = position + 1
+            """
+            print("Starting Point M = {}".format(M))
+            print("T_des = {}".format(T_des))
+            print("T_calc = {}".format(T_calc))
+            print("Did it work? {}".format(success))
+            print("arm angles = {}".format(toDegrees(thetasEnd)))
+            """
+        colour = colour + 1
+
+    return cubeHomeAngles
+
 """
 This script primarily focuses on the transformations, kinematics and inverse
 kinematics of the robot arm for METR4202. The following robot has the following
@@ -189,23 +260,24 @@ if __name__ == '__main__':
     z1 = 114 # mm height of the slew joint from origin
     y2 = 185 # mm length of armA
     y3 = 140 # mm length of armB
-    z4 = 100 # mm height of claw
-    r_6  = np.array([10, 10, 100]) # mm position of camera relative to frame{1}
+    z4 = 26.14 # mm height of claw
+    r_6  = np.array([29.104, 29.104, 285.589]) # mm position of camera relative to frame{0}
     PITCH = 1 # For the vertical axis, joint 4
 
     """ Create the initial transformation and screws for all joint frames """
     T0_1, T0_2, T1_2, T2_3, T3_4, T6_5, T1_6 = np.zeros((7, 4, 4))
 
     # Joint angles in radians. This includes a joint that does not exist in the first position
-    thetas = np.array([0., 0., 0., 0., 0.]) # thetas[0] is not a real joint. Used to so that index number is same as joint number.
+    thetas = np.array([0., 0., 0., -1 * np.pi/4, 0.]) # thetas[0] is not a real joint. Used to so that index number is same as joint number.
 
     # Relative transformation matrices
     T0_1 = mr.RpToTrans(np.eye(3), np.array([0, 0, z1]))
     T1_2 = mr.RpToTrans(np.eye(3), np.array([0, y2, 0]))
     T2_3 = mr.RpToTrans(np.eye(3), np.array([0, y3, 0]))
     R3 = rot(np.array([0, 0, 1]), thetas[3])
-    T3_4 = mr.RpToTrans(R3, np.array([0, 0, 0]))
-    T1_6 = mr.RpToTrans(np.eye(3), r_6) # Camera pose relative to frame {1}
+    T3_4 = mr.RpToTrans(R3, np.array([0, 0, z4 - z1]))
+    R4 = R3
+    T0_6 = mr.RpToTrans(R4, r_6) # Camera pose relative to frame {0}
 
     # End effector position relative to home
     M = T0_1 @ T1_2 @ T2_3 @ T3_4
@@ -240,4 +312,10 @@ if __name__ == '__main__':
 
     # Test Inverse Kinematics
     #testInverseKinematics(M, Screws)
+
+    """ Get angle values for the cube locations """
+    cubeHomeAngles = np.array(np.zeros((4, 4, 4)))
+    cubeHomeAngles = initialiseCubeAngles(cubeHomeAngles)
+
+    print(cubeHomeAngles)
 

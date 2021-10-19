c77ef91a710cbc19e400f3d8ea0e2668fc747200
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 <m.leonard@uqconnect.edu.au>
2021-10-19 15:19:55 +1000

    put servo node code into class

--- robot_ws/catkin_ws/src/servo_node/scripts/servo_control.py
+++ robot_ws/catkin_ws/src/servo_node/scripts/servo_control.py
@@ -6,25 +6,28 @@ from control_logic_node.msg import CurrentJointState, DesJointState
 
 ROBOT_FREQ = 10
 
-def cb_desired_js(data):
-    print(data)
+class Servo_Contoller:
+    
+    def cb_desired_js(data):
+            print(data)
 
-def init_planner_node():
-    rospy.init_node('actuator_controller', anonymous=False)
-    rate = rospy.Rate(ROBOT_FREQ)
+    def __init__(self):
+        rospy.init_node('actuator_controller', anonymous=False)
+        rate = rospy.Rate(ROBOT_FREQ)
 
-    # Comms with Trajectory Planner
-    rospy.Subscriber("Trajectory_DesJS", DesJointState, cb_desired_js)
+        # Comms with Trajectory Planner
+        rospy.Subscriber("Trajectory_DesJS", DesJointState, self.cb_desired_js)
 
-    # Comms with Control Logic and Forward Kinematics
-    current_joint_pub = rospy.Publisher("Actuator_CurrentJS", CurrentJointState, queue_size=10)
+        # Comms with Control Logic and Forward Kinematics
+        self.current_joint_pub = rospy.Publisher("Actuator_CurrentJS", CurrentJointState, queue_size=10)
 
     
-    while not rospy.is_shutdown():
-        rate.sleep()
+        
+
 
 if __name__ == "__main__":
     try:
-        init_planner_node()
+        Servo_Contoller()
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass

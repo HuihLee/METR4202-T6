94cb55c2f93df0b39dfab974974bc8334a2b23b8
Thomas Rooth <t.rooth@uqconnect.edu.au>
2021-10-22 13:24:08 +1000

    servo_control update

--- robot_ws/catkin_ws/src/servo_node/scripts/servo_control.py
+++ robot_ws/catkin_ws/src/servo_node/scripts/servo_control.py
@@ -1,47 +1,39 @@
 #!/usr/bin/env python3
-
 import rospy
 import RPi.GPIO as GPIO
 import time
 
 from std_msgs.msg import String
 from control_logic_node.msg import CurrentJointState, DesJointState
+from sensor_msgs.msg import JointState
 
 ROBOT_FREQ = 10
 
 class Servo_Controller:
     
-    def cb_desired_js(data):
-    from sensor_msgs.msg import JointState
+    def cb_desired_js(self, data):
+        self.pos = JointState()
+        self.pos.name = ["joint_1", "joint_2", "joint_3", "joint_4"]
+        self.pos.position = [data[0], data[1], data[2], data[3]]
+        self.pos.velocity = [0.50, 1.00, 1.00, 1.00]
 
-    pub = rospy.Publisher("desired_joint_states", JointState, queue_size = 10)
-    rospy.init_node('servo_pos', anonymous=True)
-    r = rospy.Rate(10)
- 
-    pos = JointState()
-    pos.name = ["joint_1", "joint_2". "joint_3", "joint_4"]
-    pos.position = [data[0], data[1], data[2], data[3]]
-    pos.velocity = [0.50, 1.00, 1.00, 1.00]
+        sg90 = data[4]
 
-    sg90 = data[4]
-    
-    servo_pin = 13
-    duty_cycle = 7.5     # Should be the centre for a SG90
-
-    # Configure the Pi to use pin names (i.e. BCM) and allocate I/O
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(servo_pin, GPIO.OUT)
-
-    # Create PWM channel on the servo pin with a frequency of 50Hz
-    pwm_servo = GPIO.PWM(servo_pin, 50)
-    pwm_servo.start(duty_cycle)
-
-
+    # closes the gripper when the threshold pi/2 is passed through
+    def closeGripper(self, rad):
+        if rad < 1.57:
+            self.duty_cycle = self.openPose
+        else:
+            self.duty_cycle = self.closePose
+        return self.duty_cycle
 
 
     def __init__(self):
         rospy.init_node('actuator_controller', anonymous=False)
         rate = rospy.Rate(ROBOT_FREQ)
+
+        # Comms with Dynamixels
+        self.pub = rospy.Publisher("desired_joint_states", JointState, queue_size = 10)
 
         # Comms with Trajectory Planner
         rospy.Subscriber("Trajectory_DesJS", DesJointState, self.cb_desired_js)
@@ -49,12 +41,23 @@ class Servo_Controller:
         # Comms with Control Logic and Forward Kinematics
         self.current_joint_pub = rospy.Publisher("Actuator_CurrentJS", CurrentJointState, queue_size=10)
 
+        # Duty cycle to close the gripper
+        self.openPose = 8.1
+        # Duty cycle to open the gripper
+        self.closePose = 7.2
+
+        # Configure the Pi to use pin names (i.e. BCM) and allocate I/O
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(13, GPIO.OUT)
+
+        # Create PWM channel on pin 13 with a frequency of 50Hz
+        pwm_servo = GPIO.PWM(13, 50)
+        pwm_servo.start(self.closePose) # Duty cycle of 7.2 is the gripper open pose
+
         while not rospy.is_shutdown():
-        pub.publish(pos)
-        rospy.loginfo(pos)
-        r.sleep()
-    
-    
+            self.pub.publish(self.pos)
+            pwm_servo.ChangeDutyCycle(self.duty_cycle)
+
 
 if __name__ == "__main__":
     try:

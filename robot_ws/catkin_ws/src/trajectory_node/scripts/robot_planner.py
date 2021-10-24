200920efeea2ed6512fa939d0f779d30183ef629
s4478649 <m.leonard@uqconnect.edu.au>
2021-10-25 06:34:22 +1000

    commented robot planner

--- robot_ws/catkin_ws/src/trajectory_node/scripts/robot_planner.py
+++ robot_ws/catkin_ws/src/trajectory_node/scripts/robot_planner.py
@@ -1,34 +1,45 @@
 #!/usr/bin/env python3
 
+# required imports and dependancies
 import rospy
 from std_msgs.msg import String
 from control_logic_node.msg import TargetJointStateTrajectory, TrajectoryComplete, DesJointState
 from tf.transformations import quaternion_matrix, rotation_matrix
 
-# dependancies
 from math import *
 import numpy as np
 import modern_robotics as mr
 
+# Global Variable
 ROBOT_FREQ = 10
 
+"""
+Class object handles the trajectory of robotic arm, taking the current and desired thetas to produce continous messages to the servo_node
+"""
 class Traj:
-
+    
+    # Callback that handles publishing to servo node
     def cb_control_logic(self, msg):
         Tf = msg.motionDuration #Total Time
         method = 5
         
-        thetaStart = np.array([msg.thetasCurrent[0], msg.thetasCurrent[1], msg.thetasCurrent[2], msg.thetasCurrent[3], msg.thetasCurrent[4]])
-        thetaEnd = np.array([msg.thetasTarget[0], msg.thetasTarget[1], msg.thetasTarget[2], msg.thetasTarget[3], msg.thetasTarget[4]])
+        thetaStart = np.array([msg.thetasCurrent[0], msg.thetasCurrent[1], 
+                                msg.thetasCurrent[2], msg.thetasCurrent[3], msg.thetasCurrent[4]])
+        thetaEnd = np.array([msg.thetasTarget[0], msg.thetasTarget[1], 
+                                msg.thetasTarget[2], msg.thetasTarget[3], msg.thetasTarget[4]])
         duration = msg.motionDuration
 
         dt = 0.1
         rate = rospy.Rate(1 / dt)
         N = ceil(duration/dt)
-        if N < 2:
+        
+        if N < 2:   # remove chances of dividing by 0
             N = 2
+            
+        # get all steps for trajectory
         trajectory = mr.JointTrajectory(thetaStart, thetaEnd, Tf, N, method)
         
+        # iterate through all steps
         for way_point in trajectory:
             # every dt seconds publish next joint state until N
             self.jointState.thetas = [way_point[0], way_point[1], way_point[2], way_point[3], way_point[4]]
@@ -36,10 +47,14 @@ class Traj:
             rate.sleep() #FIXME - is the necessary?
             self.servoPub.publish(self.jointState)
         
+        rate.sleep()
+        # finished moving
         self.trajComplete = True
         self.controlPub.publish(self.trajComplete)
-
+    
+    # Init trajectory planner node
     def __init__(self):
+    
         rospy.init_node('Trajectory', anonymous=True)
 
         # inti messages
@@ -54,7 +69,7 @@ class Traj:
         # Create subscribers
         rospy.Subscriber('CL_TargetJS', TargetJointStateTrajectory, self.cb_control_logic)
 
-
+# Start trajectory node
 if __name__ == "__main__":
     try:
         Traj()
